f59b06c75149ed55bf76fe9ab8f78dd816ac7b87
Christian Gitter <christian.gitter@lhind.dlh.de>
2025-12-29 14:00:22 +0100

    Adding controller methods

--- src/main/java/de/threesixgames/wizard/api/controller/GameController.java
+++ src/main/java/de/threesixgames/wizard/api/controller/GameController.java
@@ -1,6 +1,70 @@
 package de.threesixgames.wizard.api.controller;
 
+import de.threesixgames.wizard.data.GameRepository;
+import de.threesixgames.wizard.domain.cards.Card;
+import de.threesixgames.wizard.domain.cards.Rank;
+import de.threesixgames.wizard.domain.cards.Type;
+import de.threesixgames.wizard.domain.game.Game;
+import de.threesixgames.wizard.domain.players.Player;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/game")
 public class GameController {
 
-    //TODO add controller methods
+    private final GameRepository repo;
+    private final SimpMessagingTemplate messaging;
+
+    public GameController(GameRepository repo, SimpMessagingTemplate messaging) {
+        this.repo = repo;
+        this.messaging = messaging;
+    }
+
+    @PostMapping("/create")
+    public UUID create(@RequestParam String username) {
+        Game game = new Game();
+        repo.save(game);
+        join(game.getId(), username);
+        return game.getId();
+    }
+
+    @PostMapping("/{id}/join")
+    public void join(@PathVariable UUID id, @RequestParam String username) {
+        Game game = repo.getGame(id);
+        game.join(new Player(UUID.randomUUID(), username));
+        repo.save(game);
+        messaging.convertAndSend("/topic/" + id + "/joined", username);
+    }
+
+    @PostMapping("/{id}/start")
+    public void start(@PathVariable UUID id) {
+        Game game = repo.getGame(id);
+        game.start();
+        repo.save(game);
+        messaging.convertAndSend("/topic/" + id + "/started", game);
+    }
+
+    @PostMapping("/{id}/bid")
+    public void bid(@PathVariable UUID id,
+                    @RequestParam UUID playerId,
+                    @RequestParam int bid) {
+        Game game = repo.getGame(id);
+        game.placeBid(playerId, bid);
+        repo.save(game);
+        messaging.convertAndSend("/topic/" + id + "/bid", "");
+    }
+
+    @PostMapping("/{id}/play")
+    public void play(@PathVariable UUID id,
+                     @RequestParam UUID playerId,
+                     @RequestParam Type suit,
+                     @RequestParam Rank rank) {
+        Game game = repo.getGame(id);
+        game.playCard(playerId, new Card(suit, rank));
+        repo.save(game);
+        messaging.convertAndSend("/topic/" + id + "/update", "");
+    }
 }
--- src/main/java/de/threesixgames/wizard/data/InMemoryGameRepository.java
+++ src/main/java/de/threesixgames/wizard/data/InMemoryGameRepository.java
@@ -1,11 +1,13 @@
 package de.threesixgames.wizard.data;
 
 import de.threesixgames.wizard.domain.game.Game;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Repository
 public class InMemoryGameRepository implements GameRepository {
     private final Map<UUID, Game> storage = new HashMap<>();
 
